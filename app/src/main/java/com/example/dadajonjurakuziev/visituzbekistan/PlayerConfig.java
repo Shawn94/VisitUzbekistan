117df4e22f29e0a06f8ad81c054c02f3ae87be17
Dadajon Jurakuziev <dadajonjurakuziev@gmail.com>
2018-12-09 04:25:50 +0900

    Add YouTube API, YouTubeVieoPlayer

---
+++ app/src/main/java/com/example/dadajonjurakuziev/visituzbekistan/PlayerConfig.java
@@ -0,0 +1,19 @@
+package com.example.dadajonjurakuziev.visituzbekistan;
+
+public class PlayerConfig {
+
+    PlayerConfig(){
+
+    }
+
+    private static final String API_KEY = "AIzaSyAwpiyl9PunTHQbDAvtwIfSFxr-hTkPWWs";
+//    private static final String API_KEY2 = "AIzaSyDAsHqWz9-LBV0xE6L4Uu44OpIFYd1CzB0";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+
+//    public static String getApiKey2() {
+//        return API_KEY2;
+//    }
+}
--- app/src/main/java/com/example/dadajonjurakuziev/visituzbekistan/VideosActivity.java
+++ app/src/main/java/com/example/dadajonjurakuziev/visituzbekistan/VideosActivity.java
@@ -1,13 +1,85 @@
 package com.example.dadajonjurakuziev.visituzbekistan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class VideosActivity extends AppCompatActivity {
+public class VideosActivity extends YouTubeBaseActivity {
+    private static final String TAG = "VideosActivity";
+    YouTubePlayerView youTubePlayerView;
+    Button startBtn;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starting YouTubePlayerView.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
+
+        startBtn = findViewById(R.id.startBtn);
+        youTubePlayerView = findViewById(R.id.videoPlayer);
+
+
+
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+//                Log.d(TAG, "onInitializationSuccess: Done initializing.");
+//                youTubePlayer.loadVideo("W4hTJybfU7s");
+////                List<String> videoList = new ArrayList<>();
+////                videoList.add("pu4rJertDJQ");
+////                videoList.add("W4hTJybfU7s");
+////                youTubePlayer.loadVideos(videoList);
+
+                if(null== youTubePlayer) return;
+
+                // Start buffering
+                if (!b) {
+                    youTubePlayer.cueVideo("pu4rJertDJQ");
+                }
+
+                // Add listeners to YouTubePlayer instance
+                youTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override public void onAdStarted() { }
+                    @Override public void onError(YouTubePlayer.ErrorReason arg0) { }
+                    @Override public void onLoaded(String arg0) { }
+                    @Override public void onLoading() { }
+                    @Override public void onVideoEnded() { }
+                    @Override public void onVideoStarted() { }
+
+                });
+
+                youTubePlayer.setPlaybackEventListener(new YouTubePlayer.PlaybackEventListener() {
+                    @Override public void onBuffering(boolean arg0) { }
+                    @Override public void onPaused() { }
+                    @Override public void onPlaying() { }
+                    @Override public void onSeekTo(int arg0) { }
+                    @Override public void onStopped() { }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, "onInitializationFailure: Failed to initializing");
+            }
+        };
+
+        //AutoPlay
+        youTubePlayerView.initialize(PlayerConfig.getApiKey(), mOnInitializedListener);
+
+//        startBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: Initializing YouTube Player.");
+//                Toast.makeText(VideosActivity.this, "Click on Play button", Toast.LENGTH_SHORT).show();
+//                youTubePlayerView.initialize(PlayerConfig.getApiKey(), mOnInitializedListener);
+//            }
+//        });
+
     }
 }
